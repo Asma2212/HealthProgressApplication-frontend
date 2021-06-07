f2a803e59377dcf04102715e899a584cd05fc3ed
mariem202 <mariemmhiri82@gmail.com>
2021-06-07 22:24:18 +0200

    env

--- src/app/patient.service.ts
+++ src/app/patient.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Patient } from './Patien';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatientService {
-  private apiServerURL ='';
+  private apiServerURL = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) { }
   public getPatients (): Observable <Patient[]>
